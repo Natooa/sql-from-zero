b94667058c67f94aacfedf7f3e9dd950c90b996e
Nurdiyar <nurdi.instagramm@gmail.com>
2025-08-11 21:20:53 +0500

    starting learn PostgreSQL like and MySQL

---
+++ scripts/data/insert_into.sql
@@ -0,0 +1,4 @@
+INSERT INTO students(name, surname, course, email)
+VALUES ('Nurdiyar', 'Seitnurov', 3, 'nurdi.instagramm@gmail.com'),
+       ('Zhasmin', 'Makulbekova', 2, 'zhasmin.makulbekova@gmail.com'),
+       ('Abilkhan', 'Seitnurov', 1, 'abilhan2011@gmail.com');
---
+++ scripts/schema/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students(
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(50),
+                         surname VARCHAR(50),
+                         course INT,
+                         email VARCHAR(100)
+);
